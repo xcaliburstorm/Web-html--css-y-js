2c869ab1ff3b6887c13bdb4a36448d9b2e08839a
Ravenx <rad3c@hotmail.com>
2020-05-02 16:56:07 -0500

    Web page Example

---
+++ main.js
@@ -0,0 +1,12 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+  });
+
+  //Scrollreveal
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards', { delay: 500 });
+ScrollReveal().reveal('.card-banner-one', { delay: 500 });
+ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
+
+  
+
